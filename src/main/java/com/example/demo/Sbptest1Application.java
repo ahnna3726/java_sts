93becbb2d221d439242c2b0f55e5b7c752b23cb7
ahnna <ahnna3726@gmail.com>
2020-07-01 12:14:14 +0900

    first commit

---
+++ src/main/java/com/example/demo/Sbptest1Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class Sbptest1Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Sbptest1Application.class, args);
+	}
+}
---
+++ src/test/java/com/example/demo/Sbptest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbptest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
